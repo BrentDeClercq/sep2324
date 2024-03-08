9b039910c033ef4c5ba8710f61cd6ba53aaef37e
Brent De Clercq <brent.declercq2@student.hogent.be>
2024-03-08 14:21:00 +0100

    SEP2324G06-17 Installatie van httpd en toegevoegd aan firewall

---
+++ opdrachten/Linux/Vagrant/provisioning/web.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+#
+# Provisioning script for srv001
+
+#------------------------------------------------------------------------------
+# Bash settings
+#------------------------------------------------------------------------------
+
+# Enable "Bash strict mode"
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't mask errors in piped commands
+
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
+
+# Location of provisioning scripts and files
+export readonly PROVISIONING_SCRIPTS="/vagrant/provisioning/"
+# Location of files to be copied to this server
+export readonly PROVISIONING_FILES="${PROVISIONING_SCRIPTS}/files/${HOSTNAME}"
+# Network vars
+export readonly IP_ADDRESS="192.168.106.244"
+export readonly NETMASK="255.255.255.248"
+
+#------------------------------------------------------------------------------
+# "Imports"
+#------------------------------------------------------------------------------
+
+# Actions/settings common to all servers
+source ${PROVISIONING_SCRIPTS}/common.sh
+
+#------------------------------------------------------------------------------
+# Provision server
+#------------------------------------------------------------------------------
+
+log "Starting server specific provisioning tasks on ${HOSTNAME}"
+
+# Network settings
+sudo ifconfig eth1 ${IP_ADDRESS} netmask ${NETMASK}
+
+log "Installing Apache"
+
+sudo dnf install httpd -y
+
+log "Enabling httpd/Apache"
+sudo systemctl enable --now httpd
+
+log "Allow http in firewall"
+sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --reload
