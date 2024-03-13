f87ef54fabcbfa615870e154d4282ce6465c195e
Brent De Clercq <brent.declercq2@student.hogent.be>
2024-03-13 14:11:53 +0100

    SEP2324G06-30 Kleine fix voor ip adressen die niet werkt

--- opdrachten/Linux/Vagrant/provisioning/template.sh
+++ opdrachten/Linux/Vagrant/provisioning/template.sh
@@ -27,7 +27,7 @@ export readonly PROVISIONING_FILES="${PROVISIONING_SCRIPTS}/files/${HOSTNAME}"
 
 # Actions/settings common to all servers
 source ${PROVISIONING_SCRIPTS}/common.sh
-source /vagrant/vars.sh
+source "/vagrant/vars.sh"
 #------------------------------------------------------------------------------
 # Provision server
 #------------------------------------------------------------------------------
