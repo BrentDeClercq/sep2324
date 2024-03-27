0032a6e5c7a0f17b25b81e90b7586263cd9f69f4
juliedewitte <100573459+juliedewitte@users.noreply.github.com>
2024-03-27 18:22:59 +0100

    SEP2324G06-74 Installeren van RSAT tools op client-vm via powershell script

---
+++ opdrachten/Windows2/Client/config-client.ps1
@@ -0,0 +1,22 @@
+﻿# Nagaan of RSAT reeds geïnstalleerd is:
+	
+Set-ExecutionPolicy RemoteSigned
+
+#$installedFeatures = Get-WindowsCapability -Online | Where-Object { $_.Name -like 'Rsat*' -and $_.State -eq 'NotPresent' }
+$installedFeatures = Get-WindowsCapability -Online | Where-Object { $_.Name -like 'Rsat*' -and $_.State -eq 'Installed' }
+
+
+if ($installedFeatures -eq $null) {
+    Write-Host "RSAT tools are not installed. Installing..."
+
+    # RSAT tools installeren: 
+    Get-WindowsCapability -Online | Where-Object { $_.Name -like "RSAT*" } | Add-WindowsCapability -Online
+
+    Write-Host "RSAT tools have been installed."
+} 
+else {
+    Write-Host "RSAT tools are already installed."
+}
+
+pause
+
---
+++ opdrachten/Windows2/Server/server-vm.ps1
@@ -0,0 +1,19 @@
+﻿Set-ExecutionPolicy Unrestricted
+
+$vmname = "dc"
+$vmos = "Windows2022_64"
+$memory = 8192
+$disksize = 60000
+
+
+VBoxManage createvm --name $vmname --ostype $vmos --register
+
+VBoxManage modifyvm $vmname --memory $memory
+VBoxManage modifyvm $vmname --vram 128
+VBoxManage modifyvm $vmname --cpus 2
+VBoxManage modifyvm $vmname --nic1 bridged --bridgeadapter1 "TP-LINK Gigabit Ethernet USB Adapter"
+VBoxManage modifyvm $vmname --natpf1 "RDP,tcp,,3389,,3389"
+VBoxManage createhd --filename "C:\Harddisks\$vmname.vdi" --size $disksize --format VDI
+VBoxManage storagectl $vmname --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach $vmname --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\Harddisks\$vmname.vdi"
+VBoxManage storageattach $vmname --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "C:\Users\julie\SEP\sep2324-gent-g06\opdrachten\Windows\Server\en-us_windows_server_2022_x64_dvd_620d7eac.iso"
