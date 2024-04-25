724d999f1994bf5f69da373f63ed7ad329aa9031
Rayane Yousti <rayane.Yousti@student.hogent.be>
2024-04-25 14:03:10 +0200

    SSEP2324G06-117 domeinnaam aangepast bij scripts van client en server

--- opdrachten/Windows2/Server/group-policies.ps1
+++ opdrachten/Windows2/Server/group-policies.ps1
@@ -2,4 +2,4 @@
 New-GPO -Name "RestrictWorkAccounts" -Comment "Avoid for work accounts to be able to log in to DC."
 
 ##GPO koppelen aan een OU
-New-GPLink -Name "RestrictWorkAccounts" -Target "OU=DomainControllers,OU=Computers,OU=Tenurit,DC=ad,DC=g06-thematrix,DC=internal"
+New-GPLink -Name "RestrictWorkAccounts" -Target "OU=DomainControllers,OU=Computers,OU=Tenurit,DC=ad,DC=g06-tenurit,DC=internal"
