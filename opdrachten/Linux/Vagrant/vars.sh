507ea9861ae141bbb0f5de47e9c089b37b5ce1bf
Kenji De Coster <your_new_email@example.com>
2024-04-27 01:56:32 +0200

    SEP2324G06-20 o.a. testplan aangepast

--- opdrachten/Linux/Vagrant/vars.sh
+++ opdrachten/Linux/Vagrant/vars.sh
@@ -2,13 +2,13 @@
 # Network settings SEP
 #---------------------------------------------------------
 
-# # Database
-# IP_DATABASE="192.168.106.243"
-# NETMASK_DATABASE="255.255.255.248"
+# Database
+IP_DATABASE="192.168.106.243"
+NETMASK_DATABASE="255.255.255.248"
 
-# # Web
-# IP_WEB="192.168.106.244"
-# NETMASK_WEB="255.255.255.248"
+# Web
+IP_WEB="192.168.106.244"
+NETMASK_WEB="255.255.255.248"
 
 # TFTP
 IP_TFTP="192.168.106.234"
@@ -17,9 +17,9 @@ NETMASK_TFTP="255.255.255.248"
 # TFTP - test
 IP_TFTP_TEST="192.168.106.235"
 
-# # Proxy
-# IP_PROXY="192.168.106.250"
-# NETMASK_PROXY="255.255.255.252"
+# Proxy
+IP_PROXY="192.168.106.250"
+NETMASK_PROXY="255.255.255.252"
 
 # Nextcloud
 IP_NEXTCLOUD="192.168.106.245"
@@ -49,17 +49,17 @@ NETMASK_SERVERS="255.255.255.248"
 # Network settings Kenji
 #---------------------------------------------------------
 
-# Database
-IP_DATABASE="192.168.0.147"
-NETMASK_DATABASE="255.255.255.0"
+# # Database
+# IP_DATABASE="192.168.0.147"
+# NETMASK_DATABASE="255.255.255.0"
 
-# Web
-IP_WEB="192.168.0.155"
-NETMASK_WEB="255.255.255.0"
+# # Web
+# IP_WEB="192.168.0.155"
+# NETMASK_WEB="255.255.255.0"
 
-# Proxy
-IP_PROXY="192.168.0.105"
-NETMASK_PROXY="255.255.255.0"
+# # Proxy
+# IP_PROXY="192.168.0.105"
+# NETMASK_PROXY="255.255.255.0"
 
 #---------------------------------------------------------
 # Database
