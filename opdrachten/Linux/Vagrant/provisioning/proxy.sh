ed13c936b151ecc6e575bf4f0fc03b2a08f56d5a
Kenji De Coster <your_new_email@example.com>
2024-04-27 01:58:03 +0200

    SEP2324G06-20 proxy script nmap obfuscation hardening

--- opdrachten/Linux/Vagrant/provisioning/proxy.sh
+++ opdrachten/Linux/Vagrant/provisioning/proxy.sh
@@ -112,14 +112,14 @@ sudo systemctl restart nginx
 
 # Download the script from the GitHub repository
 
-# folder="/vagrant/configs/proxy"
+folder="/vagrant/configs/proxy"
 
-# curl -s https://raw.githubusercontent.com/Feriman22/portscan-protection/master/portscan-protection.sh -o $folder/portscan-protection.sh
+curl -s https://raw.githubusercontent.com/Feriman22/portscan-protection/master/portscan-protection.sh -o $folder/portscan-protection.sh
 
-# sed -i 's|WHITELISTLOCATION="/usr/local/sbin/portscan-protection-white.list"|WHITELISTLOCATION="/vagrant/configs/proxy/portscan-protection-white.list"|g' $folder/portscan-protection.sh
-# sed -i 's|SCRIPTLOCATION="/usr/local/sbin/portscan-protection.sh"|SCRIPTLOCATION="/vagrant/configs/proxy/portscan-protection.sh"|g' $folder/portscan-protection.sh
+sed -i 's|WHITELISTLOCATION="/usr/local/sbin/portscan-protection-white.list"|WHITELISTLOCATION="/vagrant/configs/proxy/portscan-protection-white.list"|g' $folder/portscan-protection.sh
+sed -i 's|SCRIPTLOCATION="/usr/local/sbin/portscan-protection.sh"|SCRIPTLOCATION="/vagrant/configs/proxy/portscan-protection.sh"|g' $folder/portscan-protection.sh
     
-# sudo bash $folder/portscan-protection.sh -i
+sudo bash $folder/portscan-protection.sh -i
 
 sudo ip route del default
 sudo ip route add default via 192.168.106.249 dev eth1
