d968549b4dee6bb8fc64676c1ef89b25fae2454d
Julie De Witte <julie.dewitte@student.hogent.be>
2024-04-26 18:52:03 +0200

    push

--- opdrachten/Windows2/Client/add-client-to-domain.ps1
+++ opdrachten/Windows2/Client/add-client-to-domain.ps1
@@ -1,7 +1,7 @@
 ##variabelen
 $Credential = Get-Credential
-$ComputerName = "DESKTOP-TU9QUJ7"
-$DomainName = "ad.g06-thematrix.internal"
+$ComputerName = "CLIENT01"
+$DomainName = "ad.g06-tenurit.internal"
 
 ##client toevoegen aan domein
 Add-Computer -ComputerName $ComputerName -DomainName $DomainName -Credential $Credential -Restart -Force
