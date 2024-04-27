2039ba317995854c36526b8171dfd1074dd64d4f
Kenji De Coster <your_new_email@example.com>
2024-04-27 02:16:50 +0200

    SEP2324G06-90 nextcloud geïntegreerd in script?

--- opdrachten/Linux/Vagrant/provisioning/proxy.sh
+++ opdrachten/Linux/Vagrant/provisioning/proxy.sh
@@ -61,7 +61,7 @@ sudo systemctl enable --now nginx
 
 # sudo systemctl reload nginx
 
-# Certificaten
+# Certificaten voor de website
 
 sudo mkdir -p /etc/nginx/ssl/
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/g06-tenurit.internal.key -out /etc/nginx/ssl/g06-tenurit.internal.crt - -subj "/C=BE/ST=Oost-Vlaanderen/L=Gent/O=TenurIT/OU=./CN=g06-tenurit.internal"
@@ -102,6 +102,47 @@ sudo echo "server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }" | sudo tee /etc/nginx/conf.d/g06-tenurit.conf > /dev/null
+
+# Certificaten voor nextcloud
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nextcloud.g06-tenurit.internal.key -out /etc/nginx/ssl/nextcloud.g06-tenurit.internal.crt - -subj "/C=BE/ST=Oost-Vlaanderen/L=Gent/O=TenurIT/OU=./CN=nextcloud.g06-tenurit.internal"
+
+sudo chmod 644 /etc/nginx/ssl/nextcloud.g06-tenurit.internal.crt
+sudo chmod 755 /etc/nginx/ssl
+
+sudo chown nginx:nginx /etc/nginx/ssl/nextcloud.g06-tenurit.internal.crt
+sudo chown nginx:nginx /etc/nginx/ssl
+
+sudo setsebool -P httpd_can_network_connect on
+
+sudo echo "server {
+    listen 80;
+    listen 443 ssl http2; #enable HTTP/2 over TLS
+    server_name nextcloud.g06-tenurit.internal www.nextcloud.g06-tenurit.internal;
+
+    ssl_certificate '/etc/nginx/ssl/nextcloud.g06-tenurit.internal.crt';
+    ssl_certificate_key '/etc/nginx/ssl/nextcloud.g06-tenurit.internal.key';
+    ssl_session_cache shared:SSL:1m;
+    ssl_session_timeout  10m;
+    ssl_ciphers PROFILE=SYSTEM;
+    ssl_prefer_server_ciphers on;
+
+    # Other SSL configuration...
+    
+    # Disable server tokens for this server block
+    server_tokens off; # disables version info
+    proxy_hide_header X-powered-By; # disables signature (nginx?)
+    add_header X-Frame-Options SAMEORIGIN; # disables clickjacking 
+
+
+    location / {
+        proxy_pass http://$IP_WEB:80;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}" | sudo tee /etc/nginx/conf.d/nextcloud.g06-tenurit.conf > /dev/null
+
 
 # Firewall configuration
 sudo firewall-cmd --zone=public --add-service=http --permanent
